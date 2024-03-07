ef85b6dac461dd83bcccd04d22ce1c1b74dd6d22
Linux <linux@Linux.myguest.virtualbox.org>
2024-03-07 13:20:08 +0400

    charger un fichier Json

--- dataTask.py
+++ dataTask.py
@@ -3,3 +3,7 @@ def save_to_json(data,filename):
 	with open(filename, 'w') as file:
 		json.dump(data,file)
 
+def load_from_json(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
