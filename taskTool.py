9e156ab53a4996eba31dc9043ad1c6fbb062f96f
vellen-govindanair <vellennair123@gmail.com>
2024-03-07 12:27:48 +0400

    Added function to delete a task

--- taskTool.py
+++ taskTool.py
@@ -7,3 +7,16 @@ def add_task(task_dict, task_name, task_description, task_deadline):
         'deadline': task_deadline,
         'completed': False
     }
+
+
+
+def delete_task(task_dict, task_name):
+    """
+    Delete a task from the task dictionary.
+    """
+    if task_name in task_dict:
+        del task_dict[task_name]
+        print(f"Task '{task_name}' deleted successfully.")
+    else:
+        print(f"Task '{task_name}' not found.")
+
